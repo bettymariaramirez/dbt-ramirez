d366616ff1c450bdf883d6b4963c48fb2489753e
bettymariaramirezderios@gmail.com <bettymariaramirezderios@gmail.com>
2022-08-07 00:58:55 +0000

    dropped provinces nulls

--- models/covid19/covid_by_city_country_province_state_week.sql
+++ models/covid19/covid_by_city_country_province_state_week.sql
@@ -5,5 +5,6 @@ select
   sum(confirmed) AS sum_confirmed,
   sum(deaths) AS sum_deaths
 from {{ source('covid19_open_data', 'compatibility_view') }}
-group by 1, 2, 3      
+WHERE province_state is not null
+group by 1, 2, 3
 order by week asc
