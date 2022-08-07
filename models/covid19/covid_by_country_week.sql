8eb1d15391d695d3031e513a65a5410d495d80d1
bettymariaramirezderios@gmail.com <bettymariaramirezderios@gmail.com>
2022-08-07 01:11:21 +0000

    updated sql

--- models/covid19/covid_by_country_week.sql
+++ models/covid19/covid_by_country_week.sql
@@ -2,6 +2,6 @@ select country_region,
        DATE_TRUNC(date, WEEK) AS week,
        sum(confirmed)         AS sum_confirmed,
        sum(deaths)            AS sum_deaths
-from `bigquery-public-data.covid19_open_data.compatibility_view`
+from {{ source('covid19_open_data', 'compatibility_view') }}
 group by 1, 2
 order by week asc
